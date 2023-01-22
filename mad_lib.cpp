6c540a5c8530e86934550a4ba1b4c087cbf5de4e
Luke Shea <shealuke@yahoo.com>
2023-01-21 21:42:49 -0600

    Built the MadLib program in cpp for fun, it's not finished yet but I will polish it up tomorrow.

---
+++ mad_lib.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string verb1;
+    string verb2;
+    string noun1;
+    string bride;
+    string verb3;
+    string adj1;
+    string city;
+    string verb4;
+    string noun2;
+    string adj2;
+    string verb5;
+    string vehicle;
+    string number;
+    string alcohol;
+    string color;
+    string body_part;
+    string noun3;
+    string silly_saying;
+    string drink;
+    string adj3;
+    string adj4;
+    string social_network;
+    string verb6;
+    string verb7;
+    string groom;
+    string feeling;
+
+
+    cout<<"Hello, and welcome to MadLibs! Enter in all the information below to see your hilarious MadLib!"<<endl;
+    cout<<"Verb: ";
+    cin>>verb1;
+    cout<<"Verb: ";
+    cin>>verb2;
+    cout<<"Noun: ";
+    cin>>noun1;
+    cout<<"Bride: ";
+    cin>>bride;
+    cout<<"Verb: ";
+    cin>>verb3;
+    cout<<"Adjective: ";
+    cin>>adj1;
+    cout<<"City: ";
+    cin>>city;
+    cout<<"Verb: ";
+    cin>>verb4;
+    cout<<"Noun: ";
+    cin>>noun2;
+    cout<<"Adjective: ";
+    cin>>adj2;
+    cout<<"Verb: ";
+    cin>>verb5;
+    cout<<"Vehicle: ";
+    cin>>vehicle;
+    cout<<"Number: ";
+    cin>>number;
+    cout<<"Alcohol: ";
+    cin>>alcohol;
+    cout<<"Color: ";
+    cin>>color;
+    cout<<"Body Part: ";
+    cin>>body_part;
+    cout<<"Noun: ";
+    cin>>noun3;
+    cout<<"Silly Saying: ";
+    cin>>silly_saying;
+    cout<<"Drink: ";
+    cin>>drink;
+    cout<<"Adjective: ";
+    cin>>adj3;
+    cout<<"Adjective: ";
+    cin>>adj4;
+    cout<<"Social Network: ";
+    cin>>social_network;
+    cout<<"Verb: ";
+    cin>>verb6;
+    cout<<"Verb: ";
+    cin>>verb7;
+    cout<<"Groom: ";
+    cin>>groom;
+    cout<<"Feeling: ";
+    cin>>feeling;
+
+    cout<<"What "<<verb1<<" at the bachelorette party, "<<verb2<<" at the bachelorette party!"<<endl;
+    cout<<"Or at least, that's what all her "<<noun1<<"'s told her. "<<bride<<"'s friends said that they wanted to have one last "<<verb3<<endl;
+    cout<<"before her days of being "<<adj1<<" were over. Their plans were to take her to "<<city<<", go "<<verb4<<"ing and maybe hire a male "<<noun2<<"er."<<endl;
+    cout<<"Pretty "<<adj2<<" stuff! First they "<<verb5<<"ed her. Then they put her in a "<<vehicle<<" and made her take "<<number<<" shots of "<<alcohol<<endl;
+    cout<<"The whole time they made her wear a "<<color<<" veil with little "<<body_part<<"'s all over it."<<endl;
+    cout<<"Since everyone out at the "<<noun3<<" knew she was a bride, people kept yelling "<<silly_saying<<" and sending over free "<<drink<<"s."<<endl;
+    cout<<"This was just the beginning of a very "<<adj3<<" and "<<adj4<<" night. Unfortunately for "<<bride<<", over the next couple of days, it was posted all over "<<social_network<<"."<<endl;
+    cout<<"Turns out what "<<verb6<<" on the bachelorette party didn't "<<verb7<<" at the bachelorette party. It's a good thing "<<groom<<" is such a "<<feeling<<"ing groom."<<endl;
+    return 0;
+}
