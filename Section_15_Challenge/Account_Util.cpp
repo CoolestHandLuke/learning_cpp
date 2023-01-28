4635f738f82d1e06822e9b1ab017f25667027460
Luke Shea <shealuke@yahoo.com>
2023-01-28 00:26:55 -0600

    Wrote all of the shell files for the Section 15 Challenge

---
+++ Section_15_Challenge/Account_Util.cpp
@@ -0,0 +1,58 @@
+#include "Account_Util.h"
+
+void display(const std::vector<Account> &accounts) {
+    std::cout<<"\nAccounts==================================================" << std::endl;
+    for (const auto &acc: accounts) {
+        std::cout << acc << std::endl;
+    }
+}
+void deposit(std::vector<Account> &accounts, double amount) {
+    std::cout<<"\nDepositing to Accounts==================================================" << std::endl;
+    for (auto &acc: accounts) {
+        if (acc.deposit(amount)) {
+            std::cout << "Deposited "<< amount << "to " << acc << std::endl;
+        } else {
+            std::cout << "Failed to deposit " << amount << "to " << acc << std::endl;
+        }
+    }
+
+}
+void withdraw(std::vector<Account> &accounts, double amount) {
+    std::cout<<"\nWithdrawing from Accounts==================================================" << std::endl;
+    for (auto &acc : accounts) {
+        if(acc.withdraw(amount)) {
+            std::cout << "Withdrew " << amount << "from " << acc << std::endl;
+        } else {
+            std::cout << "Failed to withdraw " << amount << "from " << acc << std::endl;
+        }
+    }
+
+}
+
+void display(const std::vector<Savings_Account> &accounts) {
+    std::cout<<"\nSavings Accounts==================================================" << std::endl;
+    for (const auto &acc: accounts) {
+        std::cout << acc << std::endl;
+    }
+
+}
+void deposit(std::vector<Savings_Account> &accounts, double amount) {
+    std::cout<<"\nWithdrawing from Savings Accounts==================================================" << std::endl;
+    for (auto &acc: accounts) {
+        if (acc.deposit(amount)) {
+            std::cout << "Deposited "<< amount << "to " << acc << std::endl;
+        } else {
+            std::cout << "Failed to deposit " << amount << "to " << acc << std::endl;
+        }
+    }
+}
+void withdraw(std::vector<Savings_Account> &accounts, double amount) {
+    std::cout<<"\nDepositing to Savings Accounts==================================================" << std::endl;
+    for (auto &acc : accounts) {
+        if(acc.withdraw(amount)) {
+            std::cout << "Withdrew " << amount << "from " << acc << std::endl;
+        } else {
+            std::cout << "Failed to withdraw " << amount << "from " << acc << std::endl;
+        }
+    }
+}
---
+++ Section_15_Challenge/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Savings_Account.h"
+
+
+int main() {
+
+    return 0;
+}
