5d7a02a250b1598898339c27a30005348b6007f9
Luke Shea <shealuke@yahoo.com>
2023-02-24 23:19:43 -0600

    Added in the ability to buy a seat and display the cost of said seat

--- cinema/main.cpp
+++ cinema/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main() {
 
@@ -6,41 +7,67 @@ int main() {
     int seatsPerRow;
     int totalIncome;
 
+    // Get user input
     std::cout << "Enter the number of rows: " << std::endl;
     std::cin >> rows;
     std::cout << "Enter the number of seats in each row: " << std::endl;
     std::cin >> seatsPerRow;
+    // Create the 2d array
+    // Need to add 1 to each index element to account for the row and column id's
+    std::string seats[rows + 1][seatsPerRow + 1];
 
-    if (rows * seatsPerRow <= 60) {
-        totalIncome = 10 * rows * seatsPerRow;
-    } else {    
-        totalIncome = 10 * (rows / 2) * seatsPerRow + 8 * (rows - rows / 2) * seatsPerRow;
+    for (int i = 0; i < rows + 1; i++) {
+        for (int j = 0; j < seatsPerRow + 1; j++) {
+            if (i == 0 && j == 0) {
+                seats[i][j] = ' ';
+            } else if (i == 0) {
+                seats[i][j] = std::to_string(j);
+            } else if (j == 0) {
+                seats[i][j] = std::to_string(i);
+            } else {
+                seats[i][j] = "S";
+            }
+        }
+        
     }
-    std::cout << "Total income: " << std::endl;
-    std::cout << "$" << totalIncome << std::endl;
-
-
-
-/*
+    // Print out the seats
     std::cout<<"Cinema:"<<std::endl;
-
-    char seats[8][9] = {
-        {' ', '1', '2', '3', '4', '5', '6', '7', '8'},
-        {'1', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'2', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'3', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'4', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'5', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'6', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-        {'7', 'S', 'S', 'S', 'S', 'S', 'S', 'S', 'S'},
-    };
-
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 9; j++) {
+    for (int i = 0; i < rows + 1; i++) {
+        for (int j = 0; j < seatsPerRow + 1; j++) {
             std::cout << seats[i][j] << " ";
         }
         std::cout << std::endl;
     }
-*/
+    // Ask the user for a seat to purchase
+    int userRow;
+    int userSeatInRow;
+    int ticketPrice;
+
+    std::cout << "Enter a row number:" << std::endl;
+    std::cin >> userRow;
+    std::cout << "Enter a seat number in that row:" << std::endl;
+    std::cin >> userSeatInRow;
+
+    if (rows * seatsPerRow <= 60 || userRow <= rows / 2) {
+        ticketPrice = 10;
+    } else {    
+        ticketPrice = 8;
+    }
+    // Set the bought seat to "B" and print out the seats again
+    seats[userRow][userSeatInRow] = "B";
+
+    std::cout << "\nTicket Price: $" << ticketPrice << std::endl;
+    std::cout<<"\nCinema:"<<std::endl;
+    for (int i = 0; i < rows + 1; i++) {
+        for (int j = 0; j < seatsPerRow + 1; j++) {
+            std::cout << seats[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+    
+
+    
     return 0;
 }
